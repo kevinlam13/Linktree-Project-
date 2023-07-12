cc31d336785e810c45195c4625f1656a753c4d2f
kevinlam13 <132787167+kevinlam13@users.noreply.github.com>
2023-07-11 20:00:35 -0400

    Add files via upload

---
+++ LinkCopy.js
@@ -0,0 +1,41 @@
+const shareButtons = document.querySelectorAll('.tile-share-button')
+console.log(shareButtons)
+
+
+async function copyText(e){
+
+    e.preventDefault()
+    const link = this.getAttribute('link')
+    console.log(link)
+    try { 
+       await navigator.clipboard.writeText(link)
+       alert("Copied the Link!" + link)
+    } catch (err) {
+        console.error(err)
+    }
+
+}
+shareButtons.forEach(shareButton => shareButton.addEventListener('click', copyText))
+
+
+const shareButton = document.querySelectorAll('.share-button')
+console.log(shareButtons)
+
+
+async function copyText(e){
+
+    e.preventDefault()
+    const link = this.getAttribute('link')
+    console.log(link)
+    try { 
+       await navigator.clipboard.writeText(link)
+       alert("Copied the Link!" + link)
+    } catch (err) {
+        console.error(err)
+    }
+
+}
+shareButtons.forEach(shareButton => shareButton.addEventListener('click', copyText))
+
+
+
